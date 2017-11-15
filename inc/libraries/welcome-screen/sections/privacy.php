fe5c100bda162b5565b59b598ce5f45c87a27526
Cosmin <cristea.cosmin87@gmail.com>
2017-11-15 14:38:20 +0200

    Initial version of "recommended actions" tab removed if no actions were left Recommended plugins is now togglable Privacy has an "explanation" section

--- inc/libraries/welcome-screen/sections/privacy.php
+++ inc/libraries/welcome-screen/sections/privacy.php
@@ -11,6 +11,15 @@ if ( ! defined( 'WPINC' ) ) {
 
 ?>
 <div class="privacy-section">
+	<h3><?php esc_html_e( 'We believe in a better & streamlined user experiences', 'epsilon-framework' ); ?></h3>
+	<p><?php esc_html_e( 'And as such, we\'ve made it easy for you - our user, to disable all of our theme upsells & recommendations.', 'epsilon-framework' ); ?></p>
+	<p><?php esc_html_e( 'Mind you that we use these as a way to facilitate compatible product discovery - as in: plugins that enhance the
+		user experience with any of our products. But, in the end, the user should always have a say in it.', 'epsilon-framework' ); ?> </p>
+	<br/>
+	<p><?php echo wp_kses_post( __( 'By turning any or all of the toggles below to the <span style="color: green;">ON</span> position you\'ll be able
+		to hide all upsells & recommended plugin discovery sections & actions.', 'epsilon-framework' ) ); ?></p>
+	<br/>
+	<p><?php echo wp_kses_post( __( '<u>We really hope</u> you\'ll enjoy using our products as much as we\'ve enjoyed building them.', 'epsilon-framework' ) ); ?></p>
 	<form method="post" action="options.php">
 		<?php settings_fields( 'portum-privacy' ); ?>
 		<?php foreach ( $this->privacy_options as $option ) { ?>
