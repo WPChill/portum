f2aa6b67ea01a931ff7b5b6a23d7f2206ad6c945
ngabyn <ngabyn@gmail.com>
2018-03-31 20:14:47 +0300

    * section services aligned with the rest of the code * new concept of calculating items section and section title space

--- template-parts/frontpage/services-section.php
+++ template-parts/frontpage/services-section.php
@@ -9,57 +9,145 @@
 
 $frontpage          = Epsilon_Page_Generator::get_instance( 'portum_frontpage_sections_' . get_the_ID(), get_the_ID() );
 $fields             = $frontpage->sections[ $section_id ];
-$grouping           = array(
-	'values'   => $fields['services_grouping'],
-	'group_by' => 'service_title',
-);
+$grouping           = array( 'values'   => $fields['services_grouping'], 'group_by' => 'service_title');
 $fields['services'] = $frontpage->get_repeater_field( $fields['services_repeater_field'], array(), $grouping );
 
+$attr_helper = new Epsilon_Section_Attr_Helper( $fields, 'services', Portum_Repeatable_Sections::get_instance() );
 
-$css = Epsilon_Helper::get_css_string(
-	array(
-		'background-image'    => isset( $fields['services_background_image'] ) ? $fields['services_background_image'] : '',
-		'background-position' => isset( $fields['services_background_position'] ) ? $fields['services_background_position'] : '',
-		'background-size'     => isset( $fields['services_background_size'] ) ? $fields['services_background_size'] : '',
-	)
+$parent_attr = array(
+	'id'    => $fields['services_section_unique_id'] ? array( $fields['services_section_unique_id'] ) : array(),
+	'class' => array( 'section-services', 'section', 'ewf-section', 'contrast' ),
+	'style' => array( 'background-image', 'background-position', 'background-size', 'background-repeat' ),
 );
 
-if ( ! empty( $css ) ) {
-	$css = 'style="' . $css . '"';
-}
+?>
+
+<?php 
+
+	// echo '<pre>';
+		// print_r($fields);
+	// echo '</pre>';
+		
 ?>
 
 <section data-customizer-section-id="portum_repeatable_section" data-section="<?php echo esc_attr( $section_id ); ?>">
-	<div class="section-services section contrast" <?php echo $css; ?> <?php echo ! empty( $fields['services_section_unique_id'] ) ? 'id="' . $fields['services_section_unique_id'] . '"' : ''; ?>>
-		<div class="container">
+	<div <?php $attr_helper->generate_attributes( $parent_attr ); ?>>
+		<?php
+		$attr_helper->generate_video_overlay();
+		$attr_helper->generate_color_overlay();
+		
+		$section_item_columns = 12 / intval($fields['services_column_group']);
+		$section_items_content = 12 - $section_item_columns;
+		?>
+		
+		<div class="ewf-section__content">
+		<div class="<?php echo esc_attr( Portum_Helper::container_class( 'services', $fields ) ); ?>">
+			
 			<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'services' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
-			<div class="row row-vcenter">
-				<div class="col-md-3 col-xs-4">
-					<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['services_subtitle'], $fields['services_title'] ) ); ?>
-				</div>
+			
+			
+			 
+			
+			<?php if ($fields['services_row_title_align'] == 'left'){ ?>
+				<div class="row">
+					<div class="col-md-<?php echo $section_item_columns; ?>">
+						<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['services_subtitle'], $fields['services_title'] ) ); ?>
+					</div>
 
-				<?php if ( ! empty( $fields['services'] ) ) { ?>
-					<div class="col-md-9 col-xs-8">
-						<?php foreach ( $fields['services'] as $service ) { ?>
-							<div class="col-md-4 col-xs-6">
-								<div class="services-item">
-									<?php if ( ! empty( $service['service_icon'] ) ) { ?>
-										<i class="<?php echo esc_attr( $service['service_icon'] ); ?>" aria-hidden="true"></i>
-									<?php } ?>
+					<?php if ( ! empty( $fields['services'] ) ) { ?>
+						<div class="col-md-<?php echo $section_items_content; ?>">
+							<?php foreach ( $fields['services'] as $key => $service ) { ?>
+						
+								<div class="col-md-<?php echo $section_item_columns; ?>">
+									<div class="services-item<?php echo ($key <= (intval($fields['services_column_group'])-1) ? ' services-item-first': null); ?>">
+										<?php if ( ! empty( $service['service_icon'] ) ) { ?>
+											<i class="<?php echo esc_attr( $service['service_icon'] ); ?>" aria-hidden="true"></i>
+										<?php } ?>
 
-									<?php if ( ! empty( $service['service_title'] ) ) { ?>
-										<span><?php echo esc_html( $service['service_title'] ); ?></span>
-									<?php } ?>
+										<?php if ( ! empty( $service['service_title'] ) ) { ?>
+											<span><?php echo esc_html( $service['service_title'] ); ?></span>
+										<?php } ?>
 
-									<?php if ( ! empty( $service['service_description'] ) ) { ?>
-										<strong><?php echo esc_html( $service['service_description'] ); ?></strong>
-									<?php } ?>
+										<?php if ( ! empty( $service['service_description'] ) ) { ?>
+											<strong><?php echo esc_html( $service['service_description'] ); ?></strong>
+										<?php } ?>
+									</div>
 								</div>
-							</div>
-						<?php } ?>
+							<?php } ?>
+						</div>
 					</div>
 				<?php } ?>
-			</div>
+				
+				<?php }elseif ($fields['services_row_title_align'] == 'right'){ ?>
+					<div class="row">
+						<?php if ( ! empty( $fields['services'] ) ) { ?>
+							<div class="col-md-<?php echo $section_items_content; ?>">
+								<?php foreach ( $fields['services'] as $key => $service ) { ?>
+							
+									<div class="col-md-<?php echo $section_item_columns; ?>">
+										<div class="services-item<?php echo ($key <= (intval($fields['services_column_group'])-1) ? ' services-item-first': null); ?>">
+											<?php if ( ! empty( $service['service_icon'] ) ) { ?>
+												<i class="<?php echo esc_attr( $service['service_icon'] ); ?>" aria-hidden="true"></i>
+											<?php } ?>
+
+											<?php if ( ! empty( $service['service_title'] ) ) { ?>
+												<span><?php echo esc_html( $service['service_title'] ); ?></span>
+											<?php } ?>
+
+											<?php if ( ! empty( $service['service_description'] ) ) { ?>
+												<strong><?php echo esc_html( $service['service_description'] ); ?></strong>
+											<?php } ?>
+										</div>
+									</div>
+								<?php } ?>
+							</div>
+						<?php } ?>
+					
+						<div class="col-md-<?php echo $section_item_columns; ?>">
+							<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['services_subtitle'], $fields['services_title'] ) ); ?>
+						</div>
+					</div>
+				<?php }else{ ?>
+					<div class="row">
+						<div class="col-md-12">
+							<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['services_subtitle'], $fields['services_title'] ) ); ?>
+						</div>
+					</div>
+					
+					
+					<?php if ( ! empty( $fields['services'] ) ) { ?>
+					<?php 
+						
+						$section_item_columns = 12 / intval($fields['services_column_group']);
+					
+					?>
+						<div class="row">
+							<?php foreach ( $fields['services'] as $key => $service ) { ?>
+						
+								<div class="col-md-<?php echo $section_item_columns; ?>">
+									<div class="services-item<?php echo ($key <= (intval($fields['services_column_group'])-1) ? ' -services-item-first': null); ?>">
+										<?php if ( ! empty( $service['service_icon'] ) ) { ?>
+											<i class="<?php echo esc_attr( $service['service_icon'] ); ?>" aria-hidden="true"></i>
+										<?php } ?>
+
+										<?php if ( ! empty( $service['service_title'] ) ) { ?>
+											<span><?php echo esc_html( $service['service_title'] ); ?></span>
+										<?php } ?>
+
+										<?php if ( ! empty( $service['service_description'] ) ) { ?>
+											<strong><?php echo esc_html( $service['service_description'] ); ?></strong>
+										<?php } ?>
+									</div>
+								</div>
+							<?php } ?>
+						</div>
+					<?php } ?>
+				
+				<?php }?>
+				
+				
+			
+			
 		</div>
 	</div>
 </section>
