dd7ca6fc5963cd2040cbdf0d204f7ca7bb0c3ca5
ngabyn <ngabyn@gmail.com>
2018-03-31 17:50:59 +0300

    * pricing section improvements (small css fixes) * section settings support

--- template-parts/frontpage/pricing-section.php
+++ template-parts/frontpage/pricing-section.php
@@ -30,53 +30,55 @@ $parent_attr             = array(
 		$attr_helper->generate_video_overlay();
 		$attr_helper->generate_color_overlay();
 		?>
-		<div class="<?php echo esc_attr( Portum_Helper::container_class( 'pricing', $fields ) ); ?>">
-			<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'pricing' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
+		<div class="ewf-section__content">
+			<div class="<?php echo esc_attr( Portum_Helper::container_class( 'pricing', $fields ) ); ?>">
+				<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'pricing' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
 
-			<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['pricing_subtitle'], $fields['pricing_title'] ) ); ?>
-			<div class="row">
-				<?php foreach ( $fields['pricing_boxes'] as $pricing_box ) { ?>
-					<div class="col-md-<?php echo absint( $span ); ?>">
-						<div class="pricing-item <?php echo $pricing_box['price_box_featured'] ? 'featured' : ''; ?>">
-							<div class="plan">
-								<strong>
-									<?php if ( ! empty( $pricing_box['price_box_currency'] ) ) { ?>
-										<sup><?php echo esc_html( $pricing_box['price_box_currency'] ); ?></sup>
-									<?php } ?>
+				<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['pricing_subtitle'], $fields['pricing_title'] ) ); ?>
+				<div class="row">
+					<?php foreach ( $fields['pricing_boxes'] as $pricing_box ) { ?>
+						<div class="col-md-<?php echo absint( $span ); ?>">
+							<div class="pricing-item <?php echo $pricing_box['price_box_featured'] ? 'featured' : ''; ?>">
+								<div class="plan">
+									<strong>
+										<?php if ( ! empty( $pricing_box['price_box_currency'] ) ) { ?>
+											<sup><?php echo esc_html( $pricing_box['price_box_currency'] ); ?></sup>
+										<?php } ?>
 
-									<?php
-									if ( ! empty( $pricing_box['price_box_price'] ) ) {
-										echo esc_html( $pricing_box['price_box_price'] );
-									}
-									?>
+										<?php
+										if ( ! empty( $pricing_box['price_box_price'] ) ) {
+											echo esc_html( $pricing_box['price_box_price'] );
+										}
+										?>
 
-									<?php if ( ! empty( $pricing_box['price_box_period'] ) ) { ?>
-										<sub>/<?php echo esc_html( $pricing_box['price_box_period'] ); ?></sub>
-									<?php } ?>
-								</strong>
-							</div>
-
-							<div class="details">
-								<h4><?php echo wp_kses_post( $pricing_box['price_box_title'] ); ?></h4>
-								<p><?php echo wp_kses_post( $pricing_box['price_box_text'] ); ?></p>
-							</div>
-
-							<?php if ( ! empty( $pricing_box['price_box_features'] ) ) { ?>
-
-								<?php echo wp_kses_post( $pricing_box['price_box_features'] ); ?>
-
-							<?php } ?>
-
-							<?php if ( ! empty( $pricing_box['price_box_url'] ) ) { ?>
-								<div class="wrapper">
-									<a href="<?php echo esc_url( $pricing_box['price_box_url'] ); ?>" class="btn">
-										<?php echo empty( $pricing_box['price_box_url_label'] ) ? esc_html__( 'Purchase', 'portum' ) : esc_html( $pricing_box['price_box_url_label'] ); ?>
-									</a>
+										<?php if ( ! empty( $pricing_box['price_box_period'] ) ) { ?>
+											<sub>/<?php echo esc_html( $pricing_box['price_box_period'] ); ?></sub>
+										<?php } ?>
+									</strong>
 								</div>
-							<?php } ?>
+
+								<div class="details">
+									<h4><?php echo wp_kses_post( $pricing_box['price_box_title'] ); ?></h4>
+									<p><?php echo wp_kses_post( $pricing_box['price_box_text'] ); ?></p>
+								</div>
+
+								<?php if ( ! empty( $pricing_box['price_box_features'] ) ) { ?>
+
+									<?php echo wp_kses_post( $pricing_box['price_box_features'] ); ?>
+
+								<?php } ?>
+
+								<?php if ( ! empty( $pricing_box['price_box_url'] ) ) { ?>
+									<div class="wrapper">
+										<a href="<?php echo esc_url( $pricing_box['price_box_url'] ); ?>" class="btn">
+											<?php echo empty( $pricing_box['price_box_url_label'] ) ? esc_html__( 'Purchase', 'portum' ) : esc_html( $pricing_box['price_box_url_label'] ); ?>
+										</a>
+									</div>
+								<?php } ?>
+							</div>
 						</div>
-					</div>
-				<?php } ?>
+					<?php } ?>
+				</div>
 			</div>
 		</div>
 	</div>
