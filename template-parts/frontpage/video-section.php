2d198bfb464d2a2904d2059b1079fe6c0e6fee5c
ngabyn <ngabyn@gmail.com>
2018-04-03 13:16:18 +0300

    * section video - added support for layout * section video - added support for section title, description, text

--- template-parts/frontpage/video-section.php
+++ template-parts/frontpage/video-section.php
@@ -11,21 +11,112 @@ $frontpage = Epsilon_Page_Generator::get_instance( 'portum_frontpage_sections_' 
 $fields    = $frontpage->sections[ $section_id ];
 $video     = Portum_Helper::video_type( $fields['video_id'] );
 
+$attr_helper = new Epsilon_Section_Attr_Helper( $fields, 'video', Portum_Repeatable_Sections::get_instance() );
+
+$parent_attr = array(
+	'id'    => ! empty( $fields['video_section_unique_id'] ) ? array( $fields['video_section_unique_id'] ) : array(),
+	'class' => array( 'section-video', 'section', 'ewf-section' ),
+	'style' => array( 'background-image', 'background-position', 'background-size', 'background-repeat' ),
+);
+
 wp_enqueue_style( 'plyr' );
 wp_enqueue_script( 'plyr' );
 ?>
 
 <section data-customizer-section-id="portum_repeatable_section" data-section="<?php echo esc_attr( $section_id ); ?>">
-	<div class="section-video section" <?php echo ! empty( $fields['video_section_unique_id'] ) ? 'id="' . $fields['video_section_unique_id'] . '"' : ''; ?>>
-		<?php if ( is_customize_preview() ) { ?>
-			<div class="container">
-				<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'video' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
-			</div>
-		<?php } ?>
-		<?php if ( 'none' !== $video['video_type'] ) { ?>
-			<div class="video-area auto-resizable-iframe">
-				<div data-type="<?php echo esc_attr( $video['video_type'] ); ?>" data-video-id="<?php echo esc_attr( $video['video_id'] ); ?>"></div>
-			</div>
-		<?php } ?>
+	<div <?php $attr_helper->generate_attributes( $parent_attr ); ?>>
+	<!-- <div class="section-video section" <?php echo ! empty( $fields['video_section_unique_id'] ) ? 'id="' . $fields['video_section_unique_id'] . '"' : ''; ?>> -->
+	
+		<?php
+		$attr_helper->generate_video_overlay();
+		$attr_helper->generate_color_overlay();
+		?>
+		
+		<div class="ewf-section__content">
+		<div class="<?php echo esc_attr( Portum_Helper::container_class( 'video', $fields ) ); ?>">
+			<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'video' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
+			
+			<div class="row">
+			<?php if ($fields['video_row_title_align'] == 'right'){ ?>
+				<div class="col-md-6">
+					<?php
+					echo wp_kses_post(
+						Portum_Helper::generate_section_title(
+							$fields['video_subtitle'],
+							$fields['video_title'],
+							array(
+								'doubled' => false,
+								'center'  => false,
+							)
+						)
+					);
+					?>
+					
+					<?php echo wpautop( wp_kses_post( $fields['video_text'] ) ); ?>
+				</div>
+				
+				<div class="col-md-6">
+					<div class="video-area auto-resizable-iframe">
+						<div data-type="<?php echo esc_attr( $video['video_type'] ); ?>" data-video-id="<?php echo esc_attr( $video['video_id'] ); ?>"></div>
+					</div>
+				</div>
+				
+			<?php }elseif($fields['video_row_title_align'] == 'left') { ?>
+			
+				<div class="col-md-6">
+					<div class="video-area auto-resizable-iframe">
+						<div data-type="<?php echo esc_attr( $video['video_type'] ); ?>" data-video-id="<?php echo esc_attr( $video['video_id'] ); ?>"></div>
+					</div>
+				</div>
+			
+				<div class="col-md-6">
+					<?php
+					echo wp_kses_post(
+						Portum_Helper::generate_section_title(
+							$fields['video_subtitle'],
+							$fields['video_title'],
+							array(
+								'doubled' => false,
+								'center'  => false,
+							)
+						)
+					);
+					?>
+					
+					<?php echo wpautop( wp_kses_post( $fields['video_text'] ) ); ?>
+				</div>
+				
+			<?php }else { ?>
+			
+					<div class="col-md-12">				
+						<?php
+						echo wp_kses_post(
+							Portum_Helper::generate_section_title(
+								$fields['video_subtitle'],
+								$fields['video_title'],
+								array(
+									'doubled' => false,
+									'center'  => false,
+								)
+							)
+						);
+						?>
+					
+						<?php echo wpautop( wp_kses_post( $fields['video_text'] ) ); ?>
+						
+						<?php if ( 'none' !== $video['video_type'] ) { ?>
+						<div class="video-area auto-resizable-iframe">
+							<div data-type="<?php echo esc_attr( $video['video_type'] ); ?>" data-video-id="<?php echo esc_attr( $video['video_id'] ); ?>"></div>
+						</div>
+						<?php } ?>
+					
+					</div>
+					
+				<?php } ?>
+				
+				</div>
+				
+			</div> 
+		</div>
 	</div>
 </section>
