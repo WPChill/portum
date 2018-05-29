acbdd4addea747c5d842f201af12f644fe5edce7
ngabyn <ngabyn@gmail.com>
2018-05-29 19:12:44 +0300

    * saved all

--- template-parts/frontpage/progress-section.php
+++ template-parts/frontpage/progress-section.php
@@ -46,21 +46,21 @@ $span = 12 / absint( $fields['progress_column_group'] );
 			<?php if ( 'left' === $fields['progress_row_title_align'] ) { ?>
 				<div class="row">
 
-					<div class="col-md-<?php echo esc_attr($section_item_columns); ?> col-sm-4">
+					<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?> col-sm-4">
 						<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['progress_bars_subtitle'], $fields['progress_bars_title'] ) ); ?>
 					</div>
 
 					<div class="col-md-<?php echo $section_items_content; ?> col-sm-8">
 						<div class="row">
 						<?php foreach ( $fields['progress_bars'] as $progress ) { ?>
-							<div class="col-md-<?php echo esc_attr($section_item_columns); ?>">
+							<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?>">
 								<div class="ewf-progress <?php echo 'alternate' === $progress['progress_bar_type'] ? 'ewf-progress--alternative-modern' : ''; ?>">
 
 									<h6 class="ewf-progress__title">
 										<?php
 										if ( ! empty( $progress['progress_bar_title'] ) ) {
-?>
-<?php echo esc_html( $progress['progress_bar_title'] ); ?>
+										?>
+										<?php echo esc_html( $progress['progress_bar_title'] ); ?>
 
 											<?php if ( ! empty( $progress['progress_bar_value'] ) ) { ?>
 												<span><?php echo $progress['progress_bar_value']; ?>%</span>
@@ -87,14 +87,14 @@ $span = 12 / absint( $fields['progress_column_group'] );
 					<div class="col-md-<?php echo $section_items_content; ?> col-sm-8">
 						<div class="row">
 						<?php foreach ( $fields['progress_bars'] as $progress ) { ?>
-							<div class="col-md-<?php echo esc_attr($section_item_columns); ?>">
+							<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?>">
 								<div class="ewf-progress <?php echo 'alternate' === $progress['progress_bar_type'] ? 'ewf-progress--alternative-modern' : ''; ?>">
 
 									<h6 class="ewf-progress__title">
 										<?php
 										if ( ! empty( $progress['progress_bar_title'] ) ) {
-?>
-<?php echo esc_html( $progress['progress_bar_title'] ); ?>
+										?>
+										<?php echo esc_html( $progress['progress_bar_title'] ); ?>
 
 											<?php if ( ! empty( $progress['progress_bar_value'] ) ) { ?>
 												<span><?php echo $progress['progress_bar_value']; ?>%</span>
@@ -115,7 +115,7 @@ $span = 12 / absint( $fields['progress_column_group'] );
 						</div>
 					</div>
 
-					<div class="col-md-<?php echo esc_attr($section_item_columns); ?> col-sm-4">
+					<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?> col-sm-4">
 						<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['progress_bars_subtitle'], $fields['progress_bars_title'], array( 'bottom' => true ) ) ); ?>
 					</div>
 				</div>
@@ -129,14 +129,14 @@ $span = 12 / absint( $fields['progress_column_group'] );
 
 				<div class="row">
 					<?php foreach ( $fields['progress_bars'] as $progress ) { ?>
-						<div class="col-md-<?php echo esc_attr($section_item_columns); ?>">
+						<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?>">
 							<div class="ewf-progress <?php echo  'alternate' === $progress['progress_bar_type'] ? 'ewf-progress--alternative-modern' : ''; ?>">
 
 								<h6 class="ewf-progress__title">
 									<?php
 									if ( ! empty( $progress['progress_bar_title'] ) ) {
-?>
-<?php echo esc_html( $progress['progress_bar_title'] ); ?>
+									?>
+									<?php echo esc_html( $progress['progress_bar_title'] ); ?>
 
 										<?php if ( ! empty( $progress['progress_bar_value'] ) ) { ?>
 											<span><?php echo $progress['progress_bar_value']; ?>%</span>
