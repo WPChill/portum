d8f0afe333a82d25a2d6ecf4af364c736de3c550
c0sm1n87 <cristea.cosmin87@gmail.com>
2018-05-30 22:39:50 +0300

    concept of how section styling might work with partial refresh

--- template-parts/frontpage/about-section.php
+++ template-parts/frontpage/about-section.php
@@ -17,8 +17,28 @@ $parent_attr = array(
 	'style' => array( 'background-image', 'background-position', 'background-size', 'background-repeat' ),
 );
 ?>
-
 <section data-customizer-section-id="portum_repeatable_section" data-section="<?php echo esc_attr( $section_id ); ?>">
+	<?php
+	if ( is_customize_preview() ) {
+		?>
+		<style>
+			[data-section="<?php echo esc_attr($section_id); ?>"] h1,
+			[data-section="<?php echo esc_attr($section_id); ?>"] h2,
+			[data-section="<?php echo esc_attr($section_id); ?>"] h3,
+			[data-section="<?php echo esc_attr($section_id); ?>"] h4,
+			[data-section="<?php echo esc_attr($section_id); ?>"] h5,
+			[data-section="<?php echo esc_attr($section_id); ?>"] h6,
+			[data-section="<?php echo esc_attr($section_id); ?>"] .headline span {
+				color: <?php echo esc_attr($fields['about_heading_color']); ?>
+			}
+
+			[data-section="<?php echo esc_attr($section_id); ?>"] p {
+				color: <?php echo esc_attr($fields['about_text_color']); ?>
+			}
+		</style>
+		<?php
+	}
+	?>
 	<div <?php $attr_helper->generate_attributes( $parent_attr ); ?>>
 		<?php
 		$attr_helper->generate_video_overlay();
@@ -26,74 +46,74 @@ $parent_attr = array(
 		?>
 
 		<div class="ewf-section__content">
-		<div class="<?php echo esc_attr( Portum_Helper::container_class( 'about', $fields ) ); ?>">
-		
-			<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'about' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
+			<div class="<?php echo esc_attr( Portum_Helper::container_class( 'about', $fields ) ); ?>">
 
-			<div class="row">
-				<?php if ($fields['about_row_title_align'] == 'right'){ ?>
+				<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'about' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
 
-					<?php if ( ! empty( $fields['about_image'] ) ) { ?>
-						<div class="col-md-6">
-							<?php
+				<div class="row">
+					<?php if ( $fields['about_row_title_align'] == 'right' ) { ?>
+
+						<?php if ( ! empty( $fields['about_image'] ) ) { ?>
+							<div class="col-md-6">
+								<?php
 								//about_row_title_align
 								//echo '<pre>';
 								//	print_r($fields);
 								//echo '</pre>';
+								?>
+
+								<img src="<?php echo esc_url( $fields['about_image'] ); ?>" alt=""/>
+							</div>
+						<?php } ?>
+
+						<div class="col-md-6">
+							<?php
+							echo wp_kses_post(
+								Portum_Helper::generate_section_title(
+									$fields['about_subtitle'],
+									$fields['about_title'],
+									array(
+										'doubled' => false,
+										'center'  => false,
+									)
+								)
+							);
 							?>
 
-							<img src="<?php echo esc_url( $fields['about_image'] ); ?>" alt=""/>
+							<?php echo wpautop( wp_kses_post( $fields['about_text'] ) ); ?>
 						</div>
-					<?php } ?>
-
-					<div class="col-md-6">
-						<?php
-						echo wp_kses_post(
-							Portum_Helper::generate_section_title(
-								$fields['about_subtitle'],
-								$fields['about_title'],
-								array(
-									'doubled' => false,
-									'center'  => false,
-								)
-							)
-						);
-						?>
-
-						<?php echo wpautop( wp_kses_post( $fields['about_text'] ) ); ?>
-					</div>
 
 
-				<?php }else { ?>
+					<?php } else { ?>
 
-					<div class="col-md-6">
-						<?php
-						echo wp_kses_post(
-							Portum_Helper::generate_section_title(
-								$fields['about_subtitle'],
-								$fields['about_title'],
-								array(
-									'doubled' => false,
-									'center'  => false,
-								)
-							)
-						);
-						?>
-
-						<?php echo wpautop( wp_kses_post( $fields['about_text'] ) ); ?>
-					</div>
-
-					<?php if ( ! empty( $fields['about_image'] ) ) { ?>
 						<div class="col-md-6">
-							<img src="<?php echo esc_url( $fields['about_image'] ); ?>" alt=""/>
+							<?php
+							echo wp_kses_post(
+								Portum_Helper::generate_section_title(
+									$fields['about_subtitle'],
+									$fields['about_title'],
+									array(
+										'doubled' => false,
+										'center'  => false,
+									)
+								)
+							);
+							?>
+
+							<?php echo wpautop( wp_kses_post( $fields['about_text'] ) ); ?>
 						</div>
+
+						<?php if ( ! empty( $fields['about_image'] ) ) { ?>
+							<div class="col-md-6">
+								<img src="<?php echo esc_url( $fields['about_image'] ); ?>" alt=""/>
+							</div>
+						<?php } ?>
+
 					<?php } ?>
 
-				<?php }?>
 
-
+				</div>
 			</div>
-		</div>
 		</div>
 	</div>
 </section>
