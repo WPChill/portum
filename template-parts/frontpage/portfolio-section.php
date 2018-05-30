7807750b92c4b4438a87193f379a4c0ccf945bb9
ngabyn <ngabyn@gmail.com>
2018-05-30 14:06:52 +0300

    improvements

--- template-parts/frontpage/portfolio-section.php
+++ template-parts/frontpage/portfolio-section.php
@@ -54,7 +54,7 @@ wp_enqueue_script( 'magnificPopup' );
 
 										<div class="ewf-portfolio-item__overlay">
 
-											<?php if (!$fields['portfolio_description_below']) { ?>
+											<?php if ( ! $fields['portfolio_description_below'] ) { ?>
 												<div class="ewf-portfolio-item__details">
 													<?php if ( ! empty( $item['portfolio_title'] ) ) { ?>
 														<h5><a href="<?php echo esc_url( $item['portfolio_link'] ); ?>"><?php echo esc_html( $item['portfolio_title'] ); ?></a></h5>
@@ -72,7 +72,7 @@ wp_enqueue_script( 'magnificPopup' );
 
 									</div><!-- ewf-portfolio-item__thumbnail -->
 
-									<?php if ($fields['portfolio_description_below']) { ?>
+									<?php if ( $fields['portfolio_description_below'] ) { ?>
 										<div class="ewf-portfolio-item__details">
 											<?php if ( ! empty( $item['portfolio_title'] ) ) { ?>
 												<h5><a href="<?php echo esc_url( $item['portfolio_link'] ); ?>"><?php echo esc_html( $item['portfolio_title'] ); ?></a></h5>
