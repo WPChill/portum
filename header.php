7a59ad877c1d6965e5c8529689fa10d38d758899
ngabyn <ngabyn@gmail.com>
2018-06-11 16:25:27 +0300

    General > Header > Header over content

--- header.php
+++ header.php
@@ -12,12 +12,17 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
-
 </head>
+
 <body <?php body_class( 'sticky-header' ); ?>>
 <div id="wrap">
 	<?php get_template_part( 'template-parts/header/topbar' ); ?>
-	<div id="header" <?php echo Portum::get_instance()->top_bar ? 'class="has-top-bar"' : ''; ?>>
+	<div id="header" class="
+	<?php
+		echo Portum::get_instance()->top_bar ? 'sticky--top-bar' : '';
+		echo get_theme_mod( 'portum_header_over_content', false ) ? 'sticky--over-content' : '';
+	?>
+	">
 		<!-- /// HEADER  //////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
 		<div class="container">
 			<div class="row">
