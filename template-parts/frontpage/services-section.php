acae0503e161f8f8f6b426fd2c6d3ec24b98605f
ngabyn <ngabyn@gmail.com>
2018-06-12 10:49:28 +0300

    validator - services fix

--- template-parts/frontpage/services-section.php
+++ template-parts/frontpage/services-section.php
@@ -36,42 +36,40 @@ $parent_attr = array(
 		?>
 
 		<div class="ewf-section__content">
-		<div class="<?php echo esc_attr( Portum_Helper::container_class( 'services', $fields ) ); ?>">
+			<div class="<?php echo esc_attr( Portum_Helper::container_class( 'services', $fields ) ); ?>">
 
-			<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'services' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
+				<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'services' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
 
-			<?php if ( 'left' === $fields['services_row_title_align'] ) { ?>
-				<div class="row">
-					<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?> col-sm-4">
-						<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['services_subtitle'], $fields['services_title'] ) ); ?>
-					</div>
-
-					<?php if ( ! empty( $fields['services'] ) ) { ?>
-						<div class="col-md-<?php echo $section_items_content; ?> col-sm-8">
-							<div class="row">
-							<?php foreach ( $fields['services'] as $key => $service ) { ?>
-
-								<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?> col-xs-6">
-									<div class="services-item services-item--<?php echo esc_attr( $service['service_type'] ); ?> services-item--<?php echo esc_attr( $service['service_type_color'] ); ?><?php echo ( $key <= ( intval( $fields['services_column_group'] ) - 1 ) ? ' services-item-first' : null ); ?>">
-										<?php if ( ! empty( $service['service_icon'] ) ) { ?>
-											<i class="<?php echo esc_attr( $service['service_icon'] ); ?>" aria-hidden="true"></i>
-										<?php } ?>
-
-										<?php if ( ! empty( $service['service_title'] ) ) { ?>
-											<span><?php echo esc_html( $service['service_title'] ); ?></span>
-										<?php } ?>
-
-										<?php if ( ! empty( $service['service_description'] ) ) { ?>
-											<strong><?php echo esc_html( $service['service_description'] ); ?></strong>
-										<?php } ?>
-									</div>
-								</div>
-							<?php } ?>
-							</div>
+				<?php if ( 'left' === $fields['services_row_title_align'] ) { ?>
+					<div class="row">
+						<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?> col-sm-4">
+							<?php echo wp_kses_post( Portum_Helper::generate_section_title( $fields['services_subtitle'], $fields['services_title'] ) ); ?>
 						</div>
-					</div>
-				<?php } ?>
 
+						<?php if ( ! empty( $fields['services'] ) ) { ?>
+							<div class="col-md-<?php echo $section_items_content; ?> col-sm-8">
+								<div class="row">
+								<?php foreach ( $fields['services'] as $key => $service ) { ?>
+									<div class="col-md-<?php echo esc_attr( $section_item_columns ); ?> col-xs-6">
+										<div class="services-item services-item--<?php echo esc_attr( $service['service_type'] ); ?> services-item--<?php echo esc_attr( $service['service_type_color'] ); ?><?php echo ( $key <= ( intval( $fields['services_column_group'] ) - 1 ) ? ' services-item-first' : null ); ?>">
+											<?php if ( ! empty( $service['service_icon'] ) ) { ?>
+												<i class="<?php echo esc_attr( $service['service_icon'] ); ?>" aria-hidden="true"></i>
+											<?php } ?>
+
+											<?php if ( ! empty( $service['service_title'] ) ) { ?>
+												<span><?php echo esc_html( $service['service_title'] ); ?></span>
+											<?php } ?>
+
+											<?php if ( ! empty( $service['service_description'] ) ) { ?>
+												<strong><?php echo esc_html( $service['service_description'] ); ?></strong>
+											<?php } ?>
+										</div>
+									</div>
+								<?php } ?>
+								</div>
+							</div>
+						<?php } ?>
+					</div>
 				<?php } elseif ( 'right' === $fields['services_row_title_align'] ) { ?>
 					<div class="row">
 						<?php if ( ! empty( $fields['services'] ) ) { ?>
@@ -110,11 +108,6 @@ $parent_attr = array(
 					</div>
 
 					<?php if ( ! empty( $fields['services'] ) ) { ?>
-					<?php
-
-						$section_item_columns = 12 / intval( $fields['services_column_group'] );
-
-					?>
 						<div class="row">
 							<?php foreach ( $fields['services'] as $key => $service ) { ?>
 
@@ -136,11 +129,9 @@ $parent_attr = array(
 							<?php } ?>
 						</div>
 					<?php } ?>
-
 				<?php } ?>
 
-
-
+			</div>
 		</div>
 	</div>
 </section>
