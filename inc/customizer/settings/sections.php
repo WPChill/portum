47ca7461f91416ffd134f0b9c3552e215575670a
Cristian Raiber <cristian@machothemes.com>
2018-06-12 16:51:17 +0300

    wrong text domain

--- inc/customizer/settings/sections.php
+++ inc/customizer/settings/sections.php
@@ -95,8 +95,8 @@ $sections = array(
 		'id'   => 'portum_feedback_section',
 		'args' => array(
 			'type'        => 'epsilon-section-pro',
-			'title'       => esc_html__( 'Help Improve Portum', 'sparkling' ),
-			'button_text' => esc_html__( 'Give Feedback', 'sparkling' ),
+			'title'       => esc_html__( 'Help Improve Portum', 'portum' ),
+			'button_text' => esc_html__( 'Give Feedback', 'portum' ),
 			'button_url'  => esc_url_raw( 'http://bit.ly/feedback-portum' ),
 			'priority'    => 0,
 		),
