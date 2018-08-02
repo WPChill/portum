be84e6f4ba5d05a3724ffb9714bdd464cfd05066
Cristian Raiber <cristian@machothemes.com>
2018-08-02 18:01:48 +0300

    fixed some typos and adde more text information

--- inc/customizer/settings/fields.php
+++ inc/customizer/settings/fields.php
@@ -1019,7 +1019,7 @@ Epsilon_Customizer::add_field( 'portum_advanced_slides', array(
 			),
 		),
 		'slide_background_color'        => array(
-			'label'      => esc_html__( 'Slider Overlay Color', 'portum' ),
+			'label'      => esc_html__( 'Slide Overlay Color', 'portum' ),
 			'type'       => 'epsilon-color-picker',
 			'mode'       => 'rgba',
 			'defaultVal' => '#f9f9fa',
