59b401ec4db05b639430aab606a9d4d5f40a6a92
Cristian Raiber <cristian@machothemes.com>
2018-09-12 13:58:05 +0300

    removed the option to customize the footer copyright message; it's going to be a PRO feature

--- template-parts/footer/copyright.php
+++ template-parts/footer/copyright.php
@@ -16,22 +16,19 @@ if ( get_theme_mod( 'portum_enable_copyright', true ) || has_nav_menu( 'copyrigh
 					<div id="footer-bottom-widget-area-1" class="col-sm-6 ol-xs-12">
 						<?php
 						// Translators: %s is a link.
-						echo wp_kses_post( get_theme_mod( 'portum_copyright_contents', sprintf( esc_html__( 'Colorlib &copy; %s. All rights reserved.', 'portum' ), date( 'Y' ) ) ) );
+						echo sprintf( 'Theme by: <a href="%s" target="_blank">Colorlib</a>. All rights reserved &copy %s.', esc_url( 'https://colorlib.com/' ), date( 'Y' ) );
 						?>
 					</div><!-- end .col -->
 				<?php endif; ?>
 
 				<div id="footer-bottom-widget-area-2" class="col-sm-6 col-xs-12">
 					<?php
-					wp_nav_menu(
-						array(
-							'menu'           => 'footer',
-							'theme_location' => 'footer',
-							'depth'          => 1,
-							'container'      => 'ul',
-							'menu_class'     => 'nav',
-						)
-					);
+					wp_nav_menu( array(
+						'menu'           => 'footer',
+						'theme_location' => 'footer',
+						'container'      => 'ul',
+						'menu_class'     => 'nav',
+					) );
 					?>
 				</div><!-- end .col -->
 			</div><!-- end .row -->
