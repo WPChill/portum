a17a21d78d9f0fef4bbf5d36fa26891ecfb6001c
cosmincristea <cristea.cosmin87@gmail.com>
2018-09-26 23:02:59 +0300

    Repeatable sections should be rewritten as per this example

--- template-parts/frontpage/about-section.php
+++ template-parts/frontpage/about-section.php
@@ -42,8 +42,8 @@ if ( 'left' == $fields['about_row_title_align'] || 'right' == $fields['about_row
 //end layout stuff
 ?>
 <section data-customizer-section-id="portum_repeatable_section" data-section="<?php echo esc_attr( $section_id ); ?>">
-	<?php Portum_Helper::generate_inline_css( $section_id, 'about', $fields ); ?>
-	<?php echo wp_kses( Portum_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'about' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
+	<?php //Portum_Helper::generate_inline_css( $section_id, 'about', $fields ); ?>
+	<?php echo wp_kses( Epsilon_Helper::generate_pencil( 'Portum_Repeatable_Sections', 'about' ), Epsilon_Helper::allowed_kses_pencil() ); ?>
 	<div <?php $attr_helper->generate_attributes( $parent_attr ); ?>>
 		<?php $attr_helper->generate_color_overlay(); ?>
 		<div class="ewf-section__content">
