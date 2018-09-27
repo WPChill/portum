7f0d210c616a5269d1843bcd60102bc756a3a465
giucu91 <george.ciobanu91@gmail.com>
2018-09-27 19:06:51 +0300

    Changed some sections

---
+++ inc/customizer/settings/repeatable-sections/advanced-slider.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Portum Theme Customizer repeatable section
+ *
+ * @package Portum
+ * @since   1.0
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+require_once dirname( __FILE__ ) . '/repeatable-section.php';
+
+/**
+ * Class Repeatable_Section_Advanced_Slider
+ */
+class Repeatable_Section_Advanced_Slider extends Repeatable_Section {
+	/**
+	 * Sets the section id
+	 */
+	public function set_id() {
+		$this->id = 'advanced-slider';
+	}
+
+	/**
+	 * Sets section title
+	 */
+	public function set_title() {
+		$this->title = esc_html__( 'Advanced Slider', 'portum' );
+	}
+
+	/**
+	 * Description
+	 */
+	public function set_description() {
+		$this->description = esc_html__( 'A multi-purpose slider section that you can use through-out your website.', 'portum' );
+	}
+
+	/**
+	 * Sets section image
+	 */
+	public function set_image() {
+		$this->image = esc_url( get_template_directory_uri() . '/assets/images/sections/ewf-icon-section-advanced-slider.png' );
+	}
+
+	/**
+	 * Creates the section fields
+	 */
+	public function create_fields() {
+		$this->fields = $this->normal_fields();
+	}
+
+	/**
+	 * Normal fields
+	 *
+	 * @return array
+	 */
+	public function normal_fields() {
+		return array(
+			'slider_autostart'         => array(
+				'label'       => esc_html__( 'Autostart', 'portum' ),
+				'description' => esc_html__( 'Automatically start slider after page has finished loading.', 'portum' ),
+				'type'        => 'epsilon-toggle',
+				'default'     => true,
+			),
+			'slider_infinite'          => array(
+				'label'       => esc_html__( 'Loop Slides', 'portum' ),
+				'description' => esc_html__( 'When the slider reaches the last slide, it will automatically start again from the first one.', 'portum' ),
+				'type'        => 'epsilon-toggle',
+				'default'     => true,
+			),
+			'slider_pager'             => array(
+				'label'       => esc_html__( 'Navigation Dots', 'portum' ),
+				'description' => esc_html__( 'Show slider navigation dots', 'portum' ),
+				'type'        => 'epsilon-toggle',
+				'default'     => true,
+			),
+			'slider_controls'          => array(
+				'label'       => esc_html__( 'Navigation Arrows', 'portum' ),
+				'description' => esc_html__( 'Show prev/next arrows', 'portum' ),
+				'type'        => 'epsilon-toggle',
+				'default'     => true,
+			),
+			'slider_advanced_grouping' => array(
+				'label'       => esc_html__( 'Filter shown slides', 'portum' ),
+				'description' => esc_html__( 'The items you select in here are the only ones which will be displayed on this page. Think of the information you create in a section similar to a blog post. They are all created in a single place, but filtered by category. If you want to use multiple sections and display different information in each of them, use the filtering. ', 'portum' ),
+				'type'        => 'selectize',
+				'multiple'    => true,
+				'choices'     => Portum_Helper::get_group_values_from_meta( 'portum_advanced_slides', 'slide_title' ),
+				'linking'     => array( 'portum_advanced_slides', 'slide_title' ),
+				'default'     => array( 'all' ),
+			),
+			'slider_navigation'        => array(
+				'type'            => 'epsilon-customizer-navigation',
+				'opensDoubled'    => true,
+				'navigateToId'    => 'portum_advanced_slides_section',
+				'navigateToLabel' => esc_html__( 'Add/Edit Slides &rarr;', 'portum' ),
+			),
+			'slider_repeater_field'    => array(
+				'type'    => 'hidden',
+				'default' => 'portum_advanced_slides',
+			),
+		);
+	}
+}
---
+++ inc/customizer/settings/repeatable-sections/contact.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * Portum Theme Customizer repeatable section
+ *
+ * @package Portum
+ * @since   1.0
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+require_once dirname( __FILE__ ) . '/repeatable-section.php';
+
+/**
+ * Class Repeatable_Section_Contact
+ */
+class Repeatable_Section_Contact extends Repeatable_Section {
+	/**
+	 * Sets the section id
+	 */
+	public function set_id() {
+		$this->id = 'contact';
+	}
+
+	/**
+	 * Sets section title
+	 */
+	public function set_title() {
+		$this->title = esc_html__( 'Contact Section', 'portum' );
+	}
+
+	/**
+	 * Description
+	 */
+	public function set_description() {
+		$this->description = esc_html__( 'Contact form section. You need to have a working Contact Form 7 form created.', 'portum' );
+	}
+
+	/**
+	 * Sets section image
+	 */
+	public function set_image() {
+		$this->image = esc_url( get_template_directory_uri() . '/assets/images/sections/ewf-icon-section-appointments.png' );
+	}
+
+	/**
+	 * Creates groups
+	 */
+	public function create_groups() {
+		$this->groups = array(
+			'regular'    => array(
+				'icon'  => 'dashicons dashicons-welcome-write-blog',
+				'label' => esc_html__( 'Content', 'epsilon-framework' ),
+			),
+			'background' => array(
+				'icon'  => 'dashicons dashicons-admin-customizer',
+				'label' => esc_html__( 'Background', 'epsilon-framework' ),
+			),
+			'layout'     => array(
+				'icon'  => 'dashicons dashicons-layout',
+				'label' => esc_html__( 'Layout', 'epsilon-framework' ),
+			),
+			'colors'     => array(
+				'icon'  => 'dashicons dashicons-admin-appearance',
+				'label' => esc_html__( 'Style', 'epsilon-framework' ),
+			),
+		);
+	}
+
+	/**
+	 * Creates the section fields
+	 */
+	public function create_fields() {
+		$this->fields = array_merge(
+			$this->layout_fields(),
+			$this->background_fields(),
+			$this->color_fields(),
+			$this->normal_fields()
+		);
+	}
+
+	/**
+	 * Layout fields
+	 *
+	 * @return array
+	 */
+	public function layout_fields() {
+		return array(
+			'contact_row_title_align'           => array(
+				'id'          => 'contact_row_title_align',
+				'type'        => 'select',
+				'label'       => esc_html__( 'Section Layout', 'epsilon-framework' ),
+				'description' => esc_html__( 'All sections support an alternating layout. The layout changes based on a section\'s title position. Currently available options are: title left / content right -- title center / content center -- title right / content left ', 'epsilon-framework' ),
+				'group'       => 'layout',
+				'choices'     => array(
+					'top'    => esc_html__( 'Top', 'epsilon-framework' ),
+					'bottom' => esc_html__( 'Bottom', 'epsilon-framework' ),
+					'left'   => esc_html__( 'Left', 'epsilon-framework' ),
+					'right'  => esc_html__( 'Right', 'epsilon-framework' ),
+				),
+				'default'     => ''
+			),
+			'contact_column_stretch'            => array(
+				'id'          => 'contact_column_stretch',
+				'type'        => 'select',
+				'label'       => esc_html__( 'Section Width', 'epsilon-framework' ),
+				'description' => esc_html__( 'Make the section stretch to full-width. Contained is default. There\'s also the option of boxed center. ', 'epsilon-framework' ),
+				'group'       => 'layout',
+				'choices'     => array(
+					'fullwidth' => esc_html__( 'Fullwidth (100% width)', 'epsilon-framework' ),
+					'boxedin'   => esc_html__( 'Contained (1170px width)', 'epsilon-framework' ),
+				),
+				'default'     => 'boxedin',
+			),
+			'contact_row_spacing_top'           => array(
+				'id'          => 'contact_row_spacing_top',
+				'type'        => 'select',
+				'label'       => esc_html__( 'Padding Top', 'epsilon-framework' ),
+				'description' => esc_html__( 'Adds padding top. ', 'epsilon-framework' ),
+				'group'       => 'layout',
+				'choices'     => array(
+					'lg'   => esc_html__( 'Large (105px)', 'epsilon-framework' ),
+					'md'   => esc_html__( 'Medium (75px)', 'epsilon-framework' ),
+					'sm'   => esc_html__( 'Small (35px)', 'epsilon-framework' ),
+					'none' => esc_html__( 'None (0px)', 'epsilon-framework' ),
+				),
+				'default'     => '',
+			),
+			'contact_row_spacing_bottom'        => array(
+				'id'          => 'contact_row_spacing_bottom',
+				'type'        => 'select',
+				'label'       => esc_html__( 'Padding Bottom', 'epsilon-framework' ),
+				'description' => esc_html__( 'Adds padding bottom.', 'epsilon-framework' ),
+				'group'       => 'layout',
+				'choices'     => array(
+					'lg'   => esc_html__( 'Large (105px)', 'epsilon-framework' ),
+					'md'   => esc_html__( 'Medium (75px)', 'epsilon-framework' ),
+					'sm'   => esc_html__( 'Small (35px)', 'epsilon-framework' ),
+					'none' => esc_html__( 'None (0px)', 'epsilon-framework' ),
+				),
+				'default'     => ''
+			),
+			'contact_column_alignment'          => array(
+				'id'          => 'contact_column_alignment',
+				'type'        => 'select',
+				'label'       => esc_html__( 'Horizontal Alignment', 'epsilon-framework' ),
+				'description' => esc_html__( 'Center/Left/Right align all of a sections content.', 'epsilon-framework' ),
+				'group'       => 'layout',
+				'choices'     => array(
+					'left'   => esc_html__( 'Left', 'epsilon-framework' ),
+					'center' => esc_html__( 'Center', 'epsilon-framework' ),
+					'right'  => esc_html__( 'Right', 'epsilon-framework' ),
+				),
+				'default'     => 'center'
+			),
+			'contact_column_vertical_alignment' => array(
+				'id'          => 'contact_column_vertical_alignment',
+				'type'        => 'select',
+				'label'       => esc_html__( 'Vertical Alignment', 'epsilon-framework' ),
+				'description' => esc_html__( 'We recommend leaving this to center, but feel free to experiment with the options. Top/Bottom align can be useful when you have a layout of text + image on the same line.', 'epsilon-framework' ),
+				'group'       => 'layout',
+				'choices'     => array(
+					'top'    => esc_html__( 'Top', 'epsilon-framework' ),
+					'middle' => esc_html__( 'Middle', 'epsilon-framework' ),
+					'bottom' => esc_html__( 'Bottom', 'epsilon-framework' ),
+				),
+				'default'     => 'middle'
+			),
+		);
+	}
+
+	/**
+	 * Styling fields
+	 *
+	 * @return array
+	 */
+	public function background_fields() {
+		$sizes = Epsilon_Helper::get_image_sizes();
+
+		return array(
+			'contact_background_color'    => array(
+				'id'         => 'contact_background_color',
+				'label'      => esc_html__( 'Background Color', 'epsilon-framework' ),
+				//'description' => esc_html__( 'Setting a value for this field will create a color overlay on top of background image/videos.', 'epsilon-framework' ),
+				'default'    => '',
+				'type'       => 'epsilon-color-picker',
+				'mode'       => 'rgba',
+				'defaultVal' => '',
+				'group'      => 'background',
+			),
+			'contact_background_image'    => array(
+				'id'          => 'contact_background_image',
+				'label'       => esc_html__( 'Background Image', 'epsilon-framework' ),
+				'description' => esc_html__( 'Use this field to set a background image. Content will overlay on top of the image.', 'epsilon-framework' ),
+				'type'        => 'epsilon-image',
+				'default'     => '',
+				'group'       => 'background',
+				'size'        => 'full',
+				'sizeArray'   => $sizes,
+				'mode'        => 'url',
+			),
+			'contact_background_position' => array(
+				'id'          => 'contact_background_position',
+				'label'       => esc_html__( 'Background Position', 'epsilon-framework' ),
+				'description' => esc_html__( 'We recommend using Center. Experiment with the options to see what works best for you.', 'epsilon-framwework' ),
+				'default'     => '',
+				'type'        => 'select',
+				'group'       => 'background',
+				'choices'     => array(
+					'topleft'     => __( 'Top Left', 'epsilon-framework' ),
+					'top'         => __( 'Top', 'epsilon-framework' ),
+					'topright'    => __( 'Top Right', 'epsilon-framework' ),
+					'left'        => __( 'Left', 'epsilon-framework' ),
+					'center'      => __( 'Center', 'epsilon-framework' ),
+					'right'       => __( 'Right', 'epsilon-framework' ),
+					'bottomleft'  => __( 'Bottom Left', 'epsilon-framework' ),
+					'bottom'      => __( 'Bottom', 'epsilon-framework' ),
+					'bottomright' => __( 'Bottom Right', 'epsilon-framework' ),
+				),
+			),
+			'contact_background_size'     => array(
+				'id'          => 'contact_background_size',
+				'label'       => esc_html__( 'Background Stretch', 'epsilon-framework' ),
+				'description' => esc_html__( 'We usually recommend using cover as a default option.', 'epsilon-framework' ),
+				'default'     => '',
+				'type'        => 'select',
+				'group'       => 'background',
+				'choices'     => array(
+					'cover'   => __( 'Cover', 'epsilon-framework' ),
+					'contain' => __( 'Contain', 'epsilon-framework' ),
+					'initial' => __( 'Initial', 'epsilon-framework' ),
+				),
+			),
+			'contact_background_repeat'   => array(
+				'id'          => 'contact_background_repeat',
+				'label'       => esc_html__( 'Background Repeat', 'epsilon-framework' ),
+				'description' => esc_html__( 'Set to background-repeat if you are using patterns. For parallax, we recommend setting to no-repeat.', 'epsilon-framework' ),
+				'default'     => '',
+				'type'        => 'select',
+				'group'       => 'background',
+				'choices'     => array(
+					'no-repeat' => __( 'No Repeat', 'epsilon-framework' ),
+					'repeat'    => __( 'Repeat', 'epsilon-framework' ),
+					'repeat-y'  => __( 'Repeat Y', 'epsilon-framework' ),
+					'repeat-x'  => __( 'Repeat X', 'epsilon-framework' ),
+				),
+			),
+			'contact_background_parallax' => array(
+				'id'          => 'contact_background_parallax',
+				'label'       => esc_html__( 'Background Parallax', 'epsilon-framework' ),
+				'description' => esc_html__( 'Toggling this to ON will enable the parallax effect. Make sure you have a  background image set before enabling it.', 'epsilon-framework' ),
+				'default'     => false,
+				'type'        => 'epsilon-toggle',
+				'group'       => 'background',
+			),
+		);
+	}
+
+	/**
+	 * Colors fields
+	 *
+	 * @return array
+	 */
+	public function color_fields() {
+		return array(
+			'contact_heading_color' => array(
+				'selectors'     => array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ),
+				'css-attribute' => 'color',
+				'default'       => '',
+				'label'         => __( 'Section Title Color', 'epsilon-framework' ),
+				'description'   => '',
+				'type'          => 'epsilon-color-picker',
+				'mode'          => 'hex',
+				'defaultVal'    => '',
+				'group'         => 'colors',
+			),
+			'contact_text_color'    => array(
+				'selectors'     => array( 'p' ),
+				'css-attribute' => 'color',
+				'default'       => '',
+				'label'         => __( 'Section Paragraph Color', 'epsilon-framework' ),
+				'description'   => '',
+				'type'          => 'epsilon-color-picker',
+				'mode'          => 'hex',
+				'defaultVal'    => '',
+				'group'         => 'colors',
+			),
+		);
+	}
+
+	/**
+	 * Normal fields
+	 *
+	 * @return array
+	 */
+	public function normal_fields() {
+		return array(
+			'contact_title'    => array(
+				'label'             => esc_html__( 'Title', 'portum' ),
+				'type'              => 'text',
+				'default'           => esc_html__( 'Get a quote', 'portum' ),
+				'sanitize_callback' => 'wp_kses_post',
+			),
+			'contact_subtitle' => array(
+				'label'             => esc_html__( 'Subtitle', 'portum' ),
+				'type'              => 'text',
+				'default'           => esc_html__( 'Schedule a call', 'portum' ),
+				'sanitize_callback' => 'wp_kses_post',
+			),
+			'contact_text'     => array(
+				'label'   => esc_html__( 'Description', 'portum' ),
+				'type'    => 'textarea',
+				'default' => esc_html__( 'Now it is easy and fast and you can book a consult within minutes!', 'portum' ),
+			),
+			'contact_form'     => array(
+				'label'       => esc_html__( 'Contact form', 'portum' ),
+				'description' => esc_html__( 'You need to make sure you have a Contact Form7 form created for this section to work properly.', 'portum' ),
+				'type'        => 'select',
+				'choices'     => $this->get_forms(),
+				'default'     => '',
+			),
+		);
+	}
+
+	private function get_forms() {
+		$forms = array(
+			'' => __( 'Select a Contact7 form', 'portum' ),
+		);
+		if ( defined( 'WPCF7_VERSION' ) ) {
+			/**
+			 * Get cforms, populated contact_form
+			 */
+			$args = array(
+				'post_type' => 'wpcf7_contact_form',
+			);
+
+			$posts = new WP_Query( $args );
+			wp_reset_postdata();
+			if ( $posts->have_posts() ) {
+				while ( $posts->have_posts() ) {
+					$posts->the_post();
+
+					$forms[ get_the_ID() ] = get_the_title();
+				}
+			}
+			wp_reset_query();
+		}
+
+		return $forms;
+	}
+}
