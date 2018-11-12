7f1d7dbebddad7f62a7764d9ee8b2b721b132f96
cristianraiber <cristian@machothemes.com>
2018-11-12 12:47:55 +0200

    fixed left side header changes

--- template-parts/header/menu-sidebar.php
+++ template-parts/header/menu-sidebar.php
@@ -9,17 +9,12 @@
 
 ?>
 
-<nav>
-	<input class="portum-menu-btn" type="checkbox" id="portum-menu-btn" />
-	<label class="portum-menu-icon" for="portum-menu-btn">
-		<span class="portum-navicon"></span>
-	</label>
-	<?php
-	wp_nav_menu( array(
-		'menu'           => 'primary',
-		'theme_location' => 'primary',
-		'container'      => '',
-		'menu_id'        => 'menu',
-	) );
-	?>
-</nav>
+<?php
+wp_nav_menu( array(
+	'menu'           => 'primary',
+	'theme_location' => 'primary',
+	'container'      => '',
+	'menu_id'        => 'menu',
+	'menu_class'     => 'portum-menu',
+) );
+?>
